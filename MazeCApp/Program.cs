8859f8209aebefa30118e8ce63c3023e46f81c85
Martin <jhonholtan@gmail.com>
2025-05-16 15:17:32 +0300

    changes to gen

--- MazeCApp/Program.cs
+++ MazeCApp/Program.cs
@@ -16,15 +16,17 @@ namespace MazeCApp
         static void Main(string[] args)
         {
             Console.CursorVisible = false;
-            Console.BackgroundColor = ConsoleColor.DarkGray;
+            //Console.BackgroundColor = ConsoleColor.DarkGray;
             Console.ForegroundColor = ConsoleColor.Black;
             while (true)
             {
-                    int x = 29, y = 120;
-                    //int x = 20, y = 80;
-                    Maze maze = new Maze(x, y, 1, 1);
-                    maze.FillMaze(1, y / 2, "w", 0);
-                    maze.PrintMaze();
+                int x = 29, y = 120;
+                //int x = 50, y = 50;
+                Maze maze = new Maze(x, y, 1, 1);
+                maze.FillMaze(1, y / 2, "w", 0);
+                //maze.PrintMazeInstant();
+                maze.PrintMazeSlow();
+                maze.SolveMaze();
             }
         }
     }
@@ -119,9 +121,8 @@ namespace MazeCApp
                     visited.Contains((x, y - 1)) || y - 1 < 1 ? - 1 : 3,
                     visited.Contains((x, y + 1)) || (y + 1 > width - 2) ? -1 : 1,
                     visited.Contains((x - 1, y)) || x - 1 < 1 ? - 1 : 0,
-                    visited.Contains((x - 1, y)) || x - 1 < 1 ? - 1 : 0,
                     visited.Contains((x + 1, y)) || x + 1 > heigth - 2 ? -1 : 2,
-    
+
                 };
                 maze[x, y].SetWall(pos, 1);
                 if (x == heigth - 2 && y == width / 2)
@@ -175,8 +176,9 @@ namespace MazeCApp
                 }
             }
         }
-        public void PrintMaze()
+        public void PrintMazeSlow()
         {
+            Console.BackgroundColor = ConsoleColor.DarkGray;
             //path.Push((heigth - 1, width / 2));
             SetPathMap();
             //path.Push((0, width / 2));
@@ -193,17 +195,32 @@ namespace MazeCApp
                 }
                 sw.Restart();
             }
+            Console.BackgroundColor = ConsoleColor.Black;
+        }
+        public void PrintMazeInstant()
+        {
+            //path.Push((heigth - 1, width / 2));
+            SetPathMap();
+            //path.Push((0, width / 2));
+            while (render.Count != 0)
+            {
+                (int y, int x) = render.Dequeue();
+                Console.SetCursorPosition(x, y);
+                Console.Write(maze[y, x].GetCellValue());
+            }
+        }
+        public void SolveMaze()
+        {
             var max = path.Count;
             while (path.Count != 0)
             {
-                var rgb = Rainbow(((float)path.Count/ (float)max)*2);
+                var rgb = Rainbow(((float)path.Count / (float)max) * 2);
                 Console.Write($"\x1b[38;2;{rgb.R};{rgb.G};{rgb.B}m");
                 (int y, int x) = path.Pop();
                 Console.SetCursorPosition(x, y);
                 Console.Write(maze[y, x].GetCellValue());
                 Thread.Sleep(solveSpeed);
             }
-            Console.BackgroundColor = ConsoleColor.DarkGray;
             Console.ForegroundColor = ConsoleColor.Black;
         }
         private void SetPathMap()
@@ -264,17 +281,17 @@ namespace MazeCApp
             switch ((int)div)
             {
                 case 0:
-                    return new Color( 255, ascending, 0 );
+                    return new Color(255, ascending, 0);
                 case 1:
-                    return new Color(descending, 255, 0 );
+                    return new Color(descending, 255, 0);
                 case 2:
-                    return new Color(0, 255, ascending );
+                    return new Color(0, 255, ascending);
                 case 3:
-                    return new Color(0, descending, 255 );
+                    return new Color(0, descending, 255);
                 case 4:
-                    return new Color(ascending, 0, 255 );
+                    return new Color(ascending, 0, 255);
                 default: // case 5:
-                    return new Color(255, 0, descending );
+                    return new Color(255, 0, descending);
             }
         }
         public class Color
@@ -282,7 +299,7 @@ namespace MazeCApp
             public int R;
             public int G;
             public int B;
-            public Color(int r, int g, int b) 
+            public Color(int r, int g, int b)
             {
                 this.R = r;
                 this.G = g;
